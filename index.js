e25580518b2bfcfbeb4408af1f6e14a1ce4035d2
maldonadoignacio <i.maldonado@alumno.etec.um.edu.ar>
2024-06-28 12:34:42 -0300

    Agregué documentación con JSDoc

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+/**
+ * Importa la biblioteca Express.js
+ */
 import express from 'express';
 
+/**
+ * Crea una instancia de la aplicación Express
+ */
 const app = express();
 
+/**
+ * Manejador de ruta para la ruta raíz (/)
+ * @param {object} req - Objeto de solicitud
+ * @param {object} res - Objeto de respuesta
+ */
 app.get('/', (req, res) => {
   res.send('Hello Maldonado');
 });
 
+/**
+ * Manejador de ruta para la ruta /saludo/:nombre
+ * @param {object} req - Objeto de solicitud
+ * @param {object} res - Objeto de respuesta
+ * @param {string} req.params.nombre - Parámetro de ruta "nombre"
+ */
 app.get("/saludo/:nombre", (req, res) => {
-  const nombre= req.params.nombre
-  res.send(`hola ${nombre}`)
+  const nombre = req.params.nombre;
+  res.send(`hola ${nombre}`);
 });
 
+/**
+ * Inicia la aplicación en el puerto 3000
+ */
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
